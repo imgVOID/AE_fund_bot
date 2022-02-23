0919952166437990db0208b74d5d689f478e3df4
imgVOID <imgvoid@gmail.com>
2022-02-23 14:35:54 +0200

    refactor

--- handlers/admins/scheduled_statistics_backup.py
+++ handlers/admins/scheduled_statistics_backup.py
@@ -1,10 +1,11 @@
 from time import strftime
+from datetime import datetime
 from loader import config, db
 
 
 # scheduled action, more information in the app.py file
 async def save_statistics_dump(*args):
-    date_time = (strftime("%m.%Y"), strftime("%d"))
+    date_time = (datetime.now().strftime("%m.%Y"), datetime.now().strftime("%d"))
     for group_id in config.groups:
         path = f'{config.STATISTICS_MAIN_PATH}/ID{group_id}/{date_time[0]}'
         db.backup_activity(group_id, path, date_time)
